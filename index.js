b6330a4a613598619221ff6facfb9239e881dd5b
Adebola <onedebos@gmail.com>
2019-12-06 01:29:34 +0100

    Add book to library

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 let myLibrary = [];
 
-function Book(author, title, pages,read = 0){
+function Book(title, author, pages,read = 'No'){
     this.author = author;
     this.title = title;
     this.pages = pages;
     this.read = read;
 }
 
-Book.prototype.read = function (){
-    if(this.read = 0){
-        console.log("Not read");
-    }else if(this.read=1){
-        console.log("read");
+function addBookToLibrary(){
+    let author = document.getElementById('author').value;
+    let title = document.getElementById('title').value;
+    let pages = document.getElementById('pages').value;
+    let option = document.getElementById('read');
+    let read = option.options[option.selectedIndex].text;
+    
+    const book1 = new Book(author,title,pages,read);    
+    myLibrary.push(book1);
+
+}
+
+function collectInfo(){
+    let btn = document.getElementById('add-book');
+    btn.onclick = function (){
+        addBookToLibrary();
+        console.log(myLibrary);
     }
 }
 
-// book1.prototype = Object.create(Book.prototype);
-const book1 = new Book("JRR TOlkien","THe hobbit",295);
-
-console.log(book1.author, book1.title, book1.pages, book1.read);
 
 
 
@@ -27,13 +35,19 @@ console.log(book1.author, book1.title, book1.pages, book1.read);
 let modalOpen = document.getElementById('show-modal');
 let modalClose = document.getElementById('modal-close');
 let modals = document.getElementById('modal');
+let cancelBtn  = document.getElementById('cancel');
+function closeModal (){
+    modals.classList.remove('is-active');
+}
 
 modalOpen.onclick = function (){    
     modals.classList.add('is-active');
 }
 
-modalClose.onclick = function (){
-    modals.classList.remove('is-active');
-}
+modalClose.onclick = function (){closeModal()};
+cancelBtn.onclick = function () {closeModal()};
 
 
+
+
+collectInfo();
