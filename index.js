0529ad79e60db7690162e7d3314ed375966dd7a0
Adebola <onedebos@gmail.com>
2019-12-06 01:53:23 +0100

    Show library in the DOM

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+//Library array
 let myLibrary = [];
 
+// constructor function for boook
 function Book(title, author, pages,read = 'No'){
     this.author = author;
     this.title = title;
     this.pages = pages;
     this.read = read;
 }
-
+// handle collect data from user
 function addBookToLibrary(){
-    let author = document.getElementById('author').value;
-    let title = document.getElementById('title').value;
+    let author = document.getElementById('title').value;
+    let title = document.getElementById('author').value;
     let pages = document.getElementById('pages').value;
     let option = document.getElementById('read');
     let read = option.options[option.selectedIndex].text;
@@ -19,19 +21,38 @@ function addBookToLibrary(){
 
 }
 
+// handle add to library button
 function collectInfo(){
     let btn = document.getElementById('add-book');
     btn.onclick = function (){
         addBookToLibrary();
-        console.log(myLibrary);
+        render();
     }
 }
 
 
+// display library in the DOM
+function render(){
+    let tableBody = document.getElementById('table');
+    let html = '';
+
+    for(let i=0; i<myLibrary.length; i++){
+    
+        html += '<tr>'
+        html += '<td>' + myLibrary[i].title  + '</td>'
+        html += '<td>' + myLibrary[i].author  + '</td>'
+        html += '<td>' + myLibrary[i].pages  + '</td>'
+        html += '<td>' + myLibrary[i].read  + '</td>'
+        html += '</tr>'
+
+}
+
+tableBody.innerHTML = html;
+
+}
 
 
-
-
+// handle form data collection
 let modalOpen = document.getElementById('show-modal');
 let modalClose = document.getElementById('modal-close');
 let modals = document.getElementById('modal');
@@ -49,5 +70,5 @@ cancelBtn.onclick = function () {closeModal()};
 
 
 
-
+// call functions
 collectInfo();
