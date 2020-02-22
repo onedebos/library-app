930b0d7e7b014421e6b328e43e6851a04337d9e5
Adebola <onedebos@gmail.com>
2020-02-22 12:29:46 +0100

    run app

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-//Library array
+// Library array
 const myLibrary = [];
-const table = document.getElementById("main-table");
+const table = document.getElementById('main-table');
 
 // constructor function for boook
 function Book(title, author, pages, read, bookID) {
@@ -11,70 +11,70 @@ function Book(title, author, pages, read, bookID) {
   this.bookID = bookID;
 }
 
+// implement local storage
+
+
 // handle collect data from user
 function addBookToLibrary() {
-  const author = document.getElementById("title").value;
-  const title = document.getElementById("author").value;
-  const pages = document.getElementById("pages").value;
-  const option = document.getElementById("read");
+  const author = document.getElementById('title').value;
+  const title = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const option = document.getElementById('read');
   const read = option.options[option.selectedIndex].text;
   const bookID = myLibrary.length;
 
-  if (author == "" || title == "" || pages == "") {
-    showAlert("Please fill all fields", "danger");
+  if (author == '' || title == '' || pages == '') {
+    showAlert('Please fill all fields', 'danger');
   } else {
-    showAlert("You have added a new book to your library ", "success");
+    showAlert('You have added a new book to your library ', 'success');
     const book1 = new Book(author, title, pages, read, bookID);
     myLibrary.push(book1);
     return true;
   }
 }
 
-//validation
+// validation
 function showAlert(message, className) {
-  const div = document.createElement("div");
+  const div = document.createElement('div');
 
-  if(className == 'danger'){
+  if (className == 'danger') {
     div.className = `notification is-${className} error`;
-    div.appendChild(document.createTextNode(message));    
-    const container = document.querySelector(".box");
-    const formHeading = document.querySelector(".form-header");
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.box');
+    const formHeading = document.querySelector('.form-header');
     container.insertBefore(div, formHeading);
     setTimeout(() => document.querySelector('.error').remove(), 3000);
-  
-  }else{
-     const showMsg = document.createElement('p');
-     showMsg.className = `notification is-${className} added`;
-     showMsg.appendChild(document.createTextNode(message));
-     document.querySelector(".show-notification").appendChild(showMsg);
-      //dissapear after 3s
-     setTimeout(() => document.querySelector('.added').remove(), 3000);
+  } else {
+    const showMsg = document.createElement('p');
+    showMsg.className = `notification is-${className} added`;
+    showMsg.appendChild(document.createTextNode(message));
+    document.querySelector('.show-notification').appendChild(showMsg);
+    // dissapear after 3s
+    setTimeout(() => document.querySelector('.added').remove(), 3000);
   }
- 
-
-  
 }
 
 // handle add to library button
 function collectInfo() {
-  const btn = document.getElementById("add-book");
-  btn.onclick = function() {
+  const btn = document.getElementById('add-book');
+  btn.onclick = function () {
     if (addBookToLibrary()) {
       closeModal();
       render();
       showTable();
       resetTable();
+      toggle();
     }
   };
 }
 
 // display library in the DOM
 function render() {
-  const list = document.querySelector("#table");
-  const row = document.createElement("tr");
+  const list = document.querySelector('#table');
+  const row = document.createElement('tr');
 
   for (let i = 0; i < myLibrary.length; i++) {
-    row.setAttribute("data-book-id", i);
+    row.setAttribute('data-book-id', i);
     row.innerHTML = `<td>${myLibrary[i].title}</td>
             <td>${myLibrary[i].author}</td>
             <td>${myLibrary[i].pages}</td>
@@ -92,79 +92,73 @@ function render() {
 
 function isLibraryEmpty() {
   if (myLibrary.length == 0) {
-    table.classList.add("hide");
+    table.classList.add('hide');
   }
 }
 
 function showTable() {
-  let checkLibraryText = document.getElementsByClassName("is-library-empty");
-  table.classList.remove("hide");
+  const checkLibraryText = document.getElementsByClassName('is-library-empty');
+  table.classList.remove('hide');
   if (myLibrary.length >= 1) {
     for (let i = 0; i < checkLibraryText.length; i++) {
-      checkLibraryText[i].classList.add("hide");
+      checkLibraryText[i].classList.add('hide');
     }
   }
 }
 
 function removeBk() {
-  document.querySelector("#table").addEventListener("click", e => {
+  document.querySelector('#table').addEventListener('click', (e) => {
     deleteBookFromLibrary(e.target);
   });
 }
 
 function deleteBookFromLibrary(el) {
-  if (el.classList.contains("remove-book")) {
-    const index = el.parentElement.parentElement.getAttribute("data-book-id");
-    
+  if (el.classList.contains('remove-book')) {
+    const index = el.parentElement.parentElement.getAttribute('data-book-id');
+
     myLibrary.splice(myLibrary.indexOf[index], 1);
     el.parentElement.parentElement.remove();
-    
-  }
-  showAlert(`Book removed from library`, 'warning');
-}
-
-function toggleBookStatus() {
-  let index = event.target.parentElement.parentElement.getAttribute(
-    "data-book-id"
-  );
-  if (myLibrary[index]["read"] === "Read") {
-    myLibrary[index]["read"] = "Unread";
-    event.target.classList.remove("is-light");
-    event.target.classList.add("is-dark");
-  } else {
-    myLibrary[index]["read"] = "Read";
-    event.target.classList.add("is-light");
-    event.target.classList.remove("is-dark");
+    showAlert('Book removed from library', 'warning');
   }
 }
 
+
+// toggle read status
+// function toggler(el) {
+//   const readBtn = el.target.parentElement.previousElementSibling.textContent;
+// }
+
+// function toggle() {
+//   document.querySelector('.read-status').addEventListener('click', (e) => {
+//     toggler(e.target);
+//   });
+// }
+
+// clear modal after adding to DOM
 function resetTable() {
-  let fieldsToClear = document.getElementsByClassName("input");
+  const fieldsToClear = document.getElementsByClassName('input');
   for (let i = 0; i < fieldsToClear.length; i++) {
-    fieldsToClear[i].value = "";
+    fieldsToClear[i].value = '';
   }
-
-  document.querySelector(".error").style.display = "none";
-  
 }
 
 // handle form data collection
-let modalOpen = document.getElementById("show-modal");
-let modalClose = document.getElementById("modal-close");
-let modals = document.getElementById("modal");
-let cancelBtn = document.getElementById("cancel");
+const modalOpen = document.getElementById('show-modal');
+const modalClose = document.getElementById('modal-close');
+const modals = document.getElementById('modal');
+const cancelBtn = document.getElementById('cancel');
 function closeModal() {
-  modals.classList.remove("is-active");
+  modals.classList.remove('is-active');
 }
 
-modalOpen.onclick = function() {
-  modals.classList.add("is-active");
+modalOpen.onclick = function () {
+  modals.classList.add('is-active');
 };
 
-modalClose.onclick = function() {
+modalClose.onclick = function () {
   closeModal();
 };
-cancelBtn.onclick = function() {
+cancelBtn.onclick = function () {
   closeModal();
 };
 
@@ -172,3 +166,4 @@ cancelBtn.onclick = function() {
 collectInfo();
 isLibraryEmpty();
 removeBk();
+localStorage.setItem('key', myLibrary);
